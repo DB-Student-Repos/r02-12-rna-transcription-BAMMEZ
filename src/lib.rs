9fe0770f46365c8d84c9c4026f76782007f36bd6
Bammez <Kingnuel4@gmail.com>
2024-05-04 11:37:36 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,60 @@
-#[derive(Debug, PartialEq, Eq)]
-pub struct Dna;
+#[derive(Debug, PartialEq)]
+pub struct Dna {
+    dna: String,
+}
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Rna;
+#[derive(Debug, PartialEq)]
+pub struct Rna {
+    rna: String,
+}
+
+pub fn is_valid_strand(dna: &str, allowed: &str) -> Result<bool, usize> {
+    for (n, c) in dna.chars().enumerate() {
+        if !allowed.contains(c) {
+            return Err(n);
+        }
+    }
+    Ok(true)
+}
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{dna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        if is_valid_strand(dna, "CGAT")? {
+            return Ok(Self {
+                dna: dna.to_string(),
+            });
+        }
+        Err(0)
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {self:?} into corresponding Rna");
+        let rna = self
+            .dna
+            .clone()
+            .chars()
+            .map(|c| Dna::dna_to_rna(&c).unwrap())
+            .collect::<String>();
+        Rna::new(&rna).unwrap()
+    }
+
+    fn dna_to_rna(c: &char) -> Option<char> {
+        match c {
+            'G' => Some('C'),
+            'C' => Some('G'),
+            'T' => Some('A'),
+            'A' => Some('U'),
+            _ => None,
+        }
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{rna}' string. If string contains invalid nucleotides return index of first invalid nucleotide");
+        if is_valid_strand(rna, "CGUA")? {
+            return Ok(Self {
+                rna: rna.to_string(),
+            });
+        }
+        Err(0)
     }
 }
